689819e0433fd8508cd77257288579fdca99151f
Manu8284-hub <manusharma15march2006@gmail.com>
2025-04-04 18:29:42 +0530

    completed

--- script.js
+++ script.js
@@ -14,12 +14,44 @@ s2.addEventListener("mouseenter",function(){
     if(clr=="green"){
         s2.style.backgroundColor="green";
         clr="red";
-    } else{
+    }else if(clr=="red"){
         s2.style.backgroundColor="red";
+        clr="blue";
+    } 
+    else{
+        s2.style.backgroundColor="blue";
         clr="green";
     }
     
 })
 s2.addEventListener("mouseleave",function(){
     s2.style.backgroundColor="white";
+})
+
+let s3= document.getElementById("sq3");
+s3.addEventListener("mouseenter",function(){
+    let r1=Math.floor(Math.random()*256);
+    let r2=Math.floor(Math.random()*256);
+    let r3=Math.floor(Math.random()*256);
+    s3.style.backgroundColor=`rgb(${r1},${r2},${r3})`
+})
+s3.addEventListener("mouseleave",function(){
+    s3.style.backgroundColor="white";
+})
+
+
+let s4= document.getElementById("sq4");
+s4.addEventListener("click",function(){
+    let r1=Math.floor(Math.random()*256);
+    let r2=Math.floor(Math.random()*256);
+    let r3=Math.floor(Math.random()*256);
+    s1.style.backgroundColor=`rgb(${r1},255,255)`
+    s2.style.backgroundColor=`rgb(255,${r2},255)`
+    s3.style.backgroundColor=`rgb(255,255,${r3})`
+
+})
+s4.addEventListener("mouseleave",function(){
+    s1.style.backgroundColor="white";
+    s2.style.backgroundColor="white";
+    s3.style.backgroundColor="white";
 })
